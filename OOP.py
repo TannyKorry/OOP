54ba01d1eadf9ddf6ca3127beb4aac4df5d0ebe5
TannyKorry <tannykorry@gmail.com>
2022-09-25 22:11:40 +0300

    Added the solution to 4 tasks

--- OOP.py
+++ OOP.py
@@ -18,10 +18,10 @@ class Students:
 
     def collecting_all_grades(self):
         """The function generates a complete list of grades of the student, excluding the course"""
-        grades_list = []
+        all_grades = []
         for grade in self.grades.values():
-            grades_list += grade
-        return grades_list
+            all_grades += grade
+        return all_grades
 
     def __str__(self):
         res = f'Студент \n' \
@@ -38,7 +38,7 @@ class Students:
             print('Not a Students!')
             return
         return sum(self.collecting_all_grades()) / len(self.collecting_all_grades()) < \
-            sum(other.collecting_all_grades()) / len(other.collecting_all_grades())
+               sum(other.collecting_all_grades()) / len(other.collecting_all_grades())
 
 
 class Mentors:
@@ -63,7 +63,7 @@ class Lecturer(Mentors):
             print('Not a Lecturer!')
             return
         return sum(self.collecting_all_grades()) / len(self.collecting_all_grades()) < \
-            sum(other.collecting_all_grades()) / len(other.collecting_all_grades())
+               sum(other.collecting_all_grades()) / len(other.collecting_all_grades())
 
     def __str__(self):
         res = f'Лектор \n' \
@@ -134,5 +134,35 @@ print(some_reviewer)
 print(other_reviewer)
 print(best_student)
 print(other_student)
-print(best_student > other_student)
-print(some_lecturer < other_lecturer)
+print(f'{best_student.surname} > {other_student.surname}\n{best_student > other_student}\n')
+print(f'{some_lecturer.surname} < {other_lecturer.surname}\n{some_lecturer < other_lecturer}\n')
+
+
+def course_average_st(student_list, course):
+    """calculating the average grade in the course"""
+    grades_list = []
+    for student in student_list:
+        if isinstance(student, Students):
+            for courses, grades in student.grades.items():
+                if courses == course:
+                    grades_list += grades
+                    res = round(sum(grades_list) / len(grades_list), 1)
+    return res
+
+
+print(course_average_st([best_student, other_student], 'Git'))
+
+
+def course_average_lc(lecturer_list, course):
+    """calculating the average grade in the course"""
+    grades_list = []
+    for lecturer in lecturer_list:
+        if isinstance(lecturer, Lecturer):
+            for courses, grades in lecturer.grades.items():
+                if courses == course:
+                    grades_list += grades
+                    res = round(sum(grades_list) / len(grades_list), 1)
+    return res
+
+
+print(course_average_lc([some_lecturer, other_reviewer], 'Python'))
